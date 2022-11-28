87a69222818b4dc0e36e793615764dc84206298d
GeloPerk <114014987+GeloPerk@users.noreply.github.com>
2022-11-28 09:45:32 +0200

    hm7

---
+++ index.js
@@ -0,0 +1,44 @@
+// задача 1
+// сума яблук и винограда
+
+// console.log(135 + 47)
+
+//разница
+
+// console.log(135 - 47)
+
+// // задача 2
+
+// let students = 100;
+// students = students += 50;
+// console.log(students);
+
+// задача4
+
+// Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д.
+// значення змінної value.Використовуй методи Math.floor(), Math.ceil() та
+// Math.round().Перевір що буде в консолі при значеннях 27.3 та 27.9.
+
+
+// const value = 27.5;
+// console.log(Math.floor(value))
+// console.log(Math.ceil(value))
+// console.log(Math.round(value))
+
+
+// const value02 = 27.3
+// console.log(Math.floor(value02))
+// console.log(Math.ceil(value02))
+// console.log(Math.round(value02))
+
+
+// const value03 = 27.9
+// console.log(Math.floor(value03))
+// console.log(Math.ceil(value03))
+// console.log(Math.round(value03))
+
+// задача5
+
+let prompt()
+console.log(A has B bots in stock)
+
